6af6f764de0948e28a496bbc587c1255c4769f54
Burfeind, Jan-Niklas <jan-niklas.burfeind@sennheiser.com>
2026-01-15 15:38:26 +0100

    build: Remove bash dependency
    in favor of env and sh.
    Both are available in busybox, alpine and nixos
    (as well as in every other less exotic distro)

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env sh
 #
 # YKUSH command application build script
 
--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
 echo installing ykush command...
 sudo cp -f bin/ykushcmd /usr/bin
